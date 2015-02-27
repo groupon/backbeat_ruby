234b3bb6af36899947821683b48ee4b87db421fa
Kevin Buchanan <kevaustinbuch@gmail.com>
2015-02-27 09:04:29 -0600

    adds config specs

---
+++ spec/backbeat_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+require "backbeat"
+
+describe Backbeat do
+
+  it "allows the default context to be configured" do
+    Backbeat.configure do |config|
+      config.context = :some_context
+    end
+
+    expect(Backbeat.config.context).to eq(:some_context)
+  end
+
+  it "allows the backbeat host to be configured" do
+    Backbeat.configure do |config|
+      config.host = "http://backbeat.com"
+    end
+
+    expect(Backbeat.config.host).to eq("http://backbeat.com")
+  end
+
+  it "allows the backbeat client id to be configured" do
+    Backbeat.configure do |config|
+      config.client_id = "123"
+    end
+
+    expect(Backbeat.config.client_id).to eq("123")
+  end
+
+  it "throws an error if the default context is not configured" do
+    Backbeat.configure do |config|
+      config.context = nil
+    end
+
+    expect { Backbeat.context }.to raise_error Backbeat::ContextNotConfiguredError
+
+    Backbeat.configure do |config|
+      config.context = :some_context
+    end
+
+    expect { Backbeat.context }.to_not raise_error
+  end
+end
+
